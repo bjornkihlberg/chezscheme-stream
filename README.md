970e8184928a4b9c91337c139108eb9e2a2cbb1d
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-06-09 23:43:47 +0200

    whatevs

--- README.md
+++ README.md
@@ -6,18 +6,6 @@ Lazy streams for Chez Scheme
 
 ## Core
 
-Construct a lazy pair with the `stream-cons` macro:
-
-```scheme
-(stream-cons 1 2)
-```
-
-which compiles to:
-
-```scheme
-(cons (lambda () 1) (lambda () 2))
-```
-
 Construct lazy list-like streams with the `stream` macro.
 
 ```scheme
@@ -27,10 +15,10 @@ Construct lazy list-like streams with the `stream` macro.
 which compiles to
 
 ```scheme
-(cons (lambda () 1) (lambda () (cons (lambda () 2) (lambda () (cons (lambda () 3) '())))))
+(lambda () (cons (lambda () 1) (lambda () (cons (lambda () 2) (lambda () (cons (lambda () 3) '()))))))
 ```
 
-Since streams are represented this way they can be checked with `null?` and `pair?` and values can be accessed with `car` and `cdr` (The suspensions still need to be evaluated, ofcourse).
+Since streams are represented this way they can be evaluated once to be checked with `null?` and `pair?` without touching the individual values and values can be accessed with `car` and `cdr` (The suspensions still need to be evaluated, ofcourse).
 
 ## Utility procedures
 
--- stream.scm
+++ stream.scm
@@ -2,11 +2,13 @@
   (export list->stream stream stream->list stream-append stream-cons)
   (import (chezscheme))
   (define-syntax stream-cons
-    (syntax-rules () [(_ x y) (cons (lambda () x) (lambda () y))]))
+    (syntax-rules () [(_ x xs) (lambda () (cons (lambda () x) xs))]))
   (define-syntax stream
-    (syntax-rules () [(_) '()] [(_ x xs ...) (stream-cons x (stream xs ...))]))
+    (syntax-rules ()
+      [(_) (lambda () '())]
+      [(_ x xs ...) (stream-cons x (stream xs ...))]))
   (define (stream->list xs)
-    (let loop ([acc '()] [xs xs])
+    (let loop ([acc '()] [xs (xs)])
       (if (null? xs) (reverse acc) (loop (cons ((car xs)) acc) ((cdr xs))))))
   (define (list->stream xs)
     (if (null? xs) (stream) (stream-cons (car xs) (list->stream (cdr xs)))))
@@ -15,8 +17,8 @@
       [() (stream)]
       [(xs) xs]
       [(xs . xss)
-       (if (null? xs)
-           (apply stream-append xss)
-           (cons
-             (car xs)
-             (lambda () (apply stream-append (cons ((cdr xs)) xss)))))])))
+       (let ([xs (xs)])
+         (if (null? xs)
+             (apply stream-append xss)
+             (lambda ()
+               (cons (car xs) (apply stream-append (cons (cdr xs) xss))))))])))
