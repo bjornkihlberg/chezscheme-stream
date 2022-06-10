5057eee318862edf37d520337de2d1e635b008b6
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-06-10 02:30:00 +0200

    utility procedures for fp

--- README.md
+++ README.md
@@ -24,4 +24,44 @@ Since streams are represented this way they can be evaluated once to be checked 
 
 - `stream->list`
 - `list->stream`
-- `stream-append`
+- `stream-for-each`
+- `stream-append` appends any number of streams
+
+## Utility procedures for functional programming
+
+- `stream-take` cuts off a stream after `n` elements.
+  ```scheme
+  (stream-take 3 (stream 1 2 3 4 5 6 7)) ; equivalent to (stream 1 2 3)
+  ```
+- `stream-take-while` cuts off a stream after last element passing a given predicate
+  ```scheme
+  (stream-take-while even? (stream 2 4 6 1 8 3 10)) ; equivalent to (stream 2 4 6)
+  ```
+- `stream-skip` drops `n` elements from a stream
+  ```scheme
+  (stream-skip 3 (stream 1 2 3 4 5 6 7)) ; equivalent to (stream 4 5 6 7)
+  ```
+- `stream-skip-while` drops elements from a stream while elements pass a given predicate
+  ```scheme
+  (stream-skip-while even? (stream 2 4 6 1 8 3 10)) ; equivalent to (stream 1 8 3 10)
+  ```
+- `stream-map` maps elements
+  ```scheme
+  (stream-map add1 (stream 1 2 3 4)) ; equivalent to (stream 2 3 4 5)
+  ```
+- `stream-filter` keeps elements passing a given predicate
+  ```scheme
+  (stream-filter odd? (stream 1 2 3 4 5 6 7 8 9)) ; equivalent to (stream 1 3 5 7 9)
+  ```
+- `stream-filter-map` maps and keeps truthy elements
+  ```scheme
+  (stream-filter-map (lambda (x) (and (even? x) (- x))) (stream 1 2 3 4)) ; equivalent to (stream -2 -4)
+  ```
+- `stream-iterate` builds a potentially infinite stream or until given procedure returns `#f`
+  ```scheme
+  (stream-iterate add1 0) ; equivalent to (stream 0 1 2 3 4 ...
+  ```
+- `stream-flat-map` maps individual elements to streams and appends them
+  ```scheme
+  (stream-flat-map (lambda (x) (stream 'hey 'ho x)) (stream 1 2)) ; equivalent to (stream 'hey 'ho 1 'hey 'ho 2)
+  ```
--- stream.scm
+++ stream.scm
@@ -1,5 +1,19 @@
 (library (stream)
-  (export list->stream stream stream->list stream-append stream-cons)
+  (export list->stream
+          stream
+          stream->list
+          stream-append
+          stream-cons
+          stream-filter
+          stream-filter-map
+          stream-flat-map
+          stream-for-each
+          stream-iterate
+          stream-map
+          stream-skip
+          stream-skip-while
+          stream-take
+          stream-take-while)
   (import (chezscheme))
   (define-syntax stream-cons
     (syntax-rules () [(_ x xs) (lambda () (cons (lambda () x) xs))]))
@@ -21,4 +35,73 @@
          (if (null? xs)
              (apply stream-append xss)
              (lambda ()
-               (cons (car xs) (apply stream-append (cons (cdr xs) xss))))))])))
+               (cons (car xs) (apply stream-append (cons (cdr xs) xss))))))]))
+  (define (stream-take n xs)
+    (if (positive? n)
+        (lambda ()
+          (let ([xs (xs)])
+            (if (null? xs) '() (cons (car xs) (stream-take (sub1 n) (cdr xs))))))
+        (stream)))
+  (define (stream-take-while f xs)
+    (let loop ([xs xs])
+      (lambda ()
+        (let ([xs (xs)])
+          (if (null? xs)
+              '()
+              (let ([x ((car xs))])
+                (if (f x) (cons (lambda () x) (loop (cdr xs))) '())))))))
+  (define (stream-skip n xs)
+    (if (positive? n)
+        (lambda ()
+          (let ([xs (xs)]) (if (null? xs) '() ((stream-skip (sub1 n) (cdr xs))))))
+        xs))
+  (define (stream-skip-while f xs)
+    (let loop ([xs xs])
+      (lambda ()
+        (let ([xs (xs)])
+          (if (null? xs)
+              '()
+              (let ([x ((car xs))]) (if (f x) ((loop (cdr xs))) xs)))))))
+  (define (stream-filter f xs)
+    (let loop ([xs xs])
+      (lambda ()
+        (let ([xs (xs)])
+          (if (null? xs)
+              '()
+              (let ([x ((car xs))])
+                (if (f x)
+                    (cons (lambda () x) (loop (cdr xs)))
+                    ((loop (cdr xs))))))))))
+  (define (stream-filter-map f xs . xss)
+    (let loop ([xss (cons xs xss)])
+      (lambda ()
+        (let ([xss (map (lambda (xs) (xs)) xss)])
+          (if (exists null? xss)
+              '()
+              (let* ([x (apply f (map (lambda (xs) ((car xs))) xss))])
+                (if x
+                    (cons (lambda () x) (loop (map (lambda (xs) (cdr xs)) xss)))
+                    ((loop (map (lambda (xs) (cdr xs)) xss))))))))))
+  (define (stream-iterate f x)
+    (lambda ()
+      (let loop ([x x]) (if x (cons (lambda () x) (lambda () (loop (f x)))) '()))))
+  (define (stream-map f xs . xss)
+    (let loop ([xss (cons xs xss)])
+      (lambda ()
+        (let ([xss (map (lambda (xs) (xs)) xss)])
+          (if (exists null? xss)
+              '()
+              (cons
+                (lambda () (apply f (map (lambda (xs) ((car xs))) xss)))
+                (loop (map cdr xss))))))))
+  (define (stream-for-each f xs . xss)
+    (let loop ([xss (cons xs xss)])
+      (let ([xss (map (lambda (xs) (xs)) xss)])
+        (unless (exists null? xss)
+          (apply f (map (lambda (xs) ((car xs))) xss))
+          (loop (map cdr xss))))))
+  (define (stream-flat-map f xs)
+    (let loop ([xs xs])
+      (lambda ()
+        (let ([xs (xs)])
+          (if (null? xs) '() ((stream-append (f ((car xs))) (loop (cdr xs))))))))))
